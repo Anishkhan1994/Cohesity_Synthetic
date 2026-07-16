a59a66ee13accefda231fd93430665f7dc340792
Anishkhan1994 <khan2003anish@gmail.com>
2026-07-16 15:38:39 +0530

    first

---
+++ run-tests-jar.bat
@@ -0,0 +1,19 @@
+@echo off
+
+cd /d "D:\RND\Synthetic Monitoring\onlinebanking"
+
+:loop
+echo Running tests at %date% %time%
+
+REM Run your Java program (correct CMD syntax)
+@REM "C:\Program Files\Common Files\Oracle\Java\javapath\java.exe" "@C:\Users\m7964\AppData\Local\Temp\cp_8sgybpjoghw4nyc2iucys1is0.argfile" com.onlinebanking.Main
+"C:\Program Files\Common Files\Oracle\Java\javapath\java.exe" "@C:\Users\m7964\AppData\Local\Temp\cp_8sgybpjoghw4nyc2iucys1is0.argfile" com.onlinebanking.Main
+
+
+echo Waiting for 10 minutes...
+timeout /t 600 /nobreak
+
+@REM echo Waiting for 1 minutes...
+@REM timeout /t 60 /nobreak
+
+goto loop
